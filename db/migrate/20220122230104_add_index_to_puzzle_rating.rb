4dbc4873e960d89cec2fe6bd2221a91dd66a1607
Shubham Anand <shubham@interviewbit.com>
2022-02-24 15:21:23 +0530

    reset db

--- db/migrate/20220122230104_add_index_to_puzzle_rating.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToPuzzleRating < ActiveRecord::Migration[6.1]
-  def change
-    add_index :puzzles, :rating
-  end
-end
--- db/migrate/20220122231901_fix_column_names.rb
+++
@@ -1,8 +0,0 @@
-class FixColumnNames < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :ratings, :users_id, :user_id
-    rename_column :theme_associations, :themes_id, :theme_id
-    rename_column :ratings, :type, :rating_type
-    rename_column :games, :type, :game_type
-  end
-end
