f06a5ab5e3c242161089d10def56ece9aab4752e
Shubham Anand <shubham@interviewbit.com>
2022-02-26 17:14:38 +0530

    reloacte patched

--- config/initializers/patches.rb
+++ config/initializers/patches.rb
@@ -1,1 +1,32 @@
-Dir["#{Rails.root.join('app/patches/').to_s}*.rb"].each {|file| require file }
+# Find a better way to do this
+
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def blue
+    colorize(34)
+  end
+
+  def pink
+    colorize(35)
+  end
+
+  def light_blue
+    colorize(36)
+  end
+end
