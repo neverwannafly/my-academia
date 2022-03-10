59cb8f6e2449c1ed0f0d485e06d5840b267313af
Shubham Anand <shubham@interviewbit.com>
2022-03-10 14:07:51 +0530

    another try

--- config/initializers/kimurai.rb
+++ config/initializers/kimurai.rb
@@ -1,12 +1,14 @@
+puts 'hello'
+
 Kimurai.configure do |config|
   config.colorize_logger = true
   config.log_level = :info
 
   if ENV["GOOGLE_CHROME_BIN"].present?
-    config.selenium_chrome_path = ENV["GOOGLE_CHROME_BIN"]
+    config.selenium_chrome_path = '/app/.apt/usr/bin/google-chrome'
   end
   
   if ENV["CHROME_DRIVER_BIN"].present?
-    config.chromedriver_path = ENV["CHROME_DRIVER_BIN"]
+    config.chromedriver_path = '/app/.chromedriver/bin/chromedriver'
   end
 end
