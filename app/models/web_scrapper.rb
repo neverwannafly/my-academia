b8e2d81198ae55b838837c3e840d9599ebe217ee
Shubham Anand <shubham@interviewbit.com>
2022-03-10 12:54:45 +0530

    change declaration

--- app/models/web_scrapper.rb
+++ app/models/web_scrapper.rb
@@ -1,6 +1,14 @@
 class WebScrapper < Kimurai::Base
   @engine = :selenium_chrome
 
+  if ENV["GOOGLE_CHROME_BIN"].present?
+    Selenium::WebDriver::Chrome.path = ENV["GOOGLE_CHROME_BIN"]
+  end
+  
+  if ENV["CHROME_DRIVER_BIN"].present?
+    Selenium::WebDriver::Chrome.driver_path = ENV["CHROME_DRIVER_BIN"]
+  end
+
   class << self
     attr_accessor :response
   end
--- config/initializers/selenium.rb
+++
@@ -1,7 +0,0 @@
-if ENV["GOOGLE_CHROME_BIN"].present?
-  Selenium::WebDriver::Chrome.path = ENV["GOOGLE_CHROME_BIN"]
-end
-
-if ENV["CHROME_DRIVER_BIN"].present?
-  Selenium::WebDriver::Chrome.driver_path = ENV["CHROME_DRIVER_BIN"]
-end
